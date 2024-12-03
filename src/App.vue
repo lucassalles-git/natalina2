55a7eef54d7af154dad7b57d4ad7a7a9b99bfaaf
lucassalles <lucasmssp@gmail.com>
2024-12-03 05:07:27 -0300

    section tempo limitado pronta

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 //lógica
 import Inicio from './components/inicio.vue';
+import Meio from './components/Meio.vue';
 </script>
 
 <template>
   <!-- tags ou componentes -->
    <Inicio/>
+   <Meio/>
 
 </template>
 
---
+++ src/components/Meio.vue
@@ -0,0 +1,55 @@
+<script setup>
+import presentes from "../assets/presentes.png"
+</script>
+
+<template>
+    <main>
+        <section class="tempo-limitado">
+            <div>
+                <h2>Tempo limitado</h2>
+
+                <p>Nessas festas de fim de ano mande um presente para a pessoa amada e compartilhe a alegria do Natal.
+                </p>
+            </div>
+            <img :src="presentes" alt="">
+        </section>
+    </main>
+</template>
+
+<style scoped lang="scss">
+@mixin font-style($font: "Archivo", $weight: 600, $size: 40px, $lineHeight: 48px, $color: #121214) {
+    font-family: $font;
+    font-weight: $weight;
+    font-size: $size;
+    line-height: $lineHeight;
+    color: $color;
+}
+
+main {
+    padding: 0 352px;
+}
+
+.tempo-limitado {
+    border: solid;
+    padding: 80px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+
+    div{
+        width: 477px;
+    }
+
+    h2 {
+        margin-bottom: 24px;
+        @include font-style($lineHeight: 43.52px);
+        text-align: center;
+    }
+
+    p {
+        @include font-style($weight: 400, $size: 20px, $lineHeight: 30px);
+        text-align: center;
+    }
+}
+</style>
